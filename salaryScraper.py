3392713bbbfd292cfabfc3f03fea14f10d1d8a8d
nwillhite <nwillhite@Nicks-MacBook-Pro.local>
2017-01-30 01:30:44 -0800

    added some error checking, used regex to help filer the salaries, and added comments and formatting

--- salaryScraper.py
+++ salaryScraper.py
@@ -1,43 +1,48 @@
-# import libararies
+#!/usr/bin/python
 
+#import libararies
 
 import urllib2
-from bs4 import BeautifulSoup
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 import csv
+import re
+
+from bs4 import BeautifulSoup
 from datetime import datetime
 
-#specify the url
 
-for numb in range(1,2):
+
+
+#####################################################################################################
+'''
+    specifies the urls to pull the data from, using a for loop we are able to increment through
+    the number of pages in the site as they follow the same format
+'''
+#####################################################################################################
+count = 1
+
+for numb in range(1,11):
     #url = ("http://transparentcalifornia.com/salaries/all/?page=" + str(numb))
     url = ("http://transparentcalifornia.com/salaries/all/2015/cities/?page=" + str(numb))
     page = urllib2.urlopen(url).read()
     soup = BeautifulSoup(page, 'html.parser')
 
-    print type(soup)
+    #print type(soup)
+    print count
 
-    letters = soup.find_all('td')
+    letters = soup.find_all('td')   #finds all the data within html element td
 
-    letters2 = soup.find_all('small', {'class': 'muted'})
+    letters2 = soup.find_all('small', {'class': 'muted'})   #takes all the data with element small with class muted
 
-'''
-    Allows us to pull only the job titles out of the of the table instead of the title and city in one.
-'''
-    j = 0
-    for a in letters:
-        just_title = a.find_all('a')
-        for a in just_title:
-            if ((a.get_text() == '(See note)') and (j % 3) == 1):
-                break
-            elif (j % 3) == 1:
-                print a.get_text()
-            j += 1
 
-'''
-variables used
-'''
-    i = 0
 
+    #####################################################################################################
+    '''
+        variables used
+    '''
+    #####################################################################################################
     name = []
     job_title = []
     location = []
@@ -49,55 +54,110 @@ variables used
     totalVar = 0
     totalVar_string = ""
 
+
+    #####################################################################################################
+    '''
+        Allows us to pull only the job titles out of the of the table instead of the title and city in one.
+    '''
+    #####################################################################################################
+    j = 0
+    for a in letters:
+        just_title = a.find_all('a')
+        for a in just_title:
+            if (a.get_text() == '(See note)') and (j % 3) == 1:
+                break
+            elif (j % 3) == 1:
+                job_title.append(a.get_text())
+            j += 1
+
+
+
+    #####################################################################################################
+    '''
+        Allows for us to pull the location of the salary
+    '''
+    #####################################################################################################
     for small in letters2:
         locations = small.find_all('a')
         for a in locations:
-            location.append(a.get_text())
+            tmplocation = a.get_text()
+            tmplocation = re.sub('[,0-9]', '', tmplocation) # takes out the , 2015 from location
+            location.append(tmplocation)
 
+
+
+    #####################################################################################################
+    '''
+        parses the name, reg pay, overtime pay, other pay, and adds them together for total sum
+    '''
+    #####################################################################################################
+    i = 0
     for item in letters:
-        if ((i % 7) == 0):
+        errorCheck = item.get_text()
+        if (i % 7) == 0:
             name.append(item.get_text())
-        if ((i % 7) == 1):
-            job_title.append(item.get_text())
 
-        if ((i % 7) == 2):
+        #if ((i % 7) == 1):
+            #job_title.append(item.get_text())
+
+        if ((errorCheck == 'Aggregate') and (i % 7) == 2) or ((errorCheck == 'Not provided') and (i % 7) == 2):
+            reg_pay.append(0)
+            totalVar = 0
+
+        elif (i % 7) == 2:
             reg_pay.append(item.get_text())
-
             totalVar_string = item.get_text()
-            totalVar_string = totalVar_string[1:]
-            totalVar = float(totalVar_string.replace(',', ''))
+            totalVar_string = re.sub('[()$,]', '', totalVar_string)
+            totalVar = float(totalVar_string)
 
-        if ((i % 7) == 3):
+        if ((errorCheck == 'Aggregate') and (i % 7) == 3) or ((errorCheck == 'Not provided') and (i % 7) == 3):
+            overtime_pay.append(0)
+            totalVar += 0
+        elif (i % 7) == 3:
             overtime_pay.append(item.get_text())
-
             totalVar_string = item.get_text()
-            totalVar_string = totalVar_string[1:]
-            totalVar += float(totalVar_string.replace(',', ''))
+            totalVar_string = re.sub('[()$,]', '', totalVar_string)
+            totalVar += float(totalVar_string)
 
-        if ((i % 7) == 4):
+        if ((errorCheck == 'Aggregate') and (i % 7) == 4) or ((errorCheck == 'Not provided') and (i % 7) == 4):
+            other_pay.append(0)
+            totalVar += 0
+            total_pay.append(totalVar)
+
+        elif (i % 7) == 4:
             other_pay.append(item.get_text())
 
             totalVar_string = item.get_text()
-            totalVar_string = totalVar_string[1:]
-            totalVar += float(totalVar_string.replace(',', ''))
+            totalVar_string = re.sub('[()$,]', '', totalVar_string)
+            totalVar += float(totalVar_string)
+
             totalVar_string = '${:,.2f}'.format(totalVar)
             total_pay.append(totalVar_string)
+
         i += 1
 
+    count +=1
+    #####################################################################################################
+    '''
+        Print functions to help test that we are getting the right output
+    '''
+    #####################################################################################################
+    #for item in locations:
+    #   location.append(item.get_text())  # .encode('ascii', 'ignore'))
 
-    for item in locations:
-        location.append(item.get_text().encode('ascii', 'ignore'))
+    #for t in range(len(name)):
+    #   print name[t]
 
 
-    #for i in range(len(location)):
-    #    print location[i]
+    #####################################################################################################
+    '''
+        Handles the inputting of the data into our .csv file
+    '''
+    #####################################################################################################
 
-'''
-     # open a csv file with append, so old data will not be erased
+
+    # open a csv file with append, so old data will not be erased
     with open('index.csv', 'a') as csv_file:
         writer = csv.writer(csv_file)
         for item in range(0, len(job_title)):
             writer.writerow([name[item], job_title[item], location[item], reg_pay[item], overtime_pay[item], other_pay[item], total_pay[item], datetime.now()])
-            #writer.writerow([location[item]])
-
-'''
